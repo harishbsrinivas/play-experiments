025efaf6342d2d0b2d809249254f9a8781330d4a
Harish.B.Srinivas <harishbsrinivas@gmail.com>
2013-04-10 06:25:37 -0400

    Added dashboard. Configured app to load default dataset from test-data.yml

--- zentasks/app/controllers/Application.java
+++ zentasks/app/controllers/Application.java
@@ -2,13 +2,18 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
+import static play.data.Form.*;
 
+import models.*;
 import views.html.*;
 
 public class Application extends Controller {
-  
+
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render( 
+            Project.find.all(),
+            Task.find.all()
+        )); 
     }
-  
 }
